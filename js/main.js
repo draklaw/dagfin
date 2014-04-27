0334c71ef600442cea91afe98bfeae2a5b63f063
Doc <doc@nfor.fr>
2014-04-27 14:16:54 +0200

    Now comes with about 6*Math.PI% cooler zombie glances.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ var LEFT  = 3;
 var ZOMBIE_SHAMBLE_VELOCITY = 40;
 var ZOMBIE_CHARGE_VELOCITY = 400;
 var ZOMBIE_SPOTTING_RANGE = 160;
+var ZOMBIE_SPOTTING_ANGLE = Math.PI / 12;
 var ZOMBIE_SPOTTING_DELAY = 50;
 var ZOMBIE_IDEA_DELAY = 5000;
 
@@ -124,7 +125,9 @@ GameState.prototype.create = function () {
 		this.mobs[i].spot = function () {
 			var zed = that.mobs[j];
 			var glance = new Phaser.Line(zed.x, zed.y, that.player.x, that.player.y);
-			if (glance.length < ZOMBIE_SPOTTING_RANGE && !that.obstructed(glance))
+			// Don't ask, lest the zombie glance at YOU instead.
+			if (glance.length < ZOMBIE_SPOTTING_RANGE && !that.obstructed(glance)
+			&& Math.abs((glance.angle+3*Math.PI/2)-(2*Math.PI-zed.body.angle)) < ZOMBIE_SPOTTING_ANGLE)
 			{
 				zed.looks = BERZERK;
 			}
